ae6948c787c9dc04b63ee5d2b3bc48cbcc362dd0
Dy0mar <kurtudins@gmail.com>
2019-02-08 12:56:15 +0200

    settings apply

--- queue_task/settings.py
+++ queue_task/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tasks',
+    'app_tasks',
 ]
 
 
@@ -132,3 +132,7 @@ REDIS_PORT = '6379'
 BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
 CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+
+CELERYD_CONCURRENCY = 4
+CELERY_TRACK_STARTED = True
+CELERY_IGNORE_RESULT = False
