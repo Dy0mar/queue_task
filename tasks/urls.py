10c09b57312f9da9ea60c4ebf3400b888975b5ee
Dy0mar <kurtudins@gmail.com>
2019-02-08 10:39:02 +0200

    modify url

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,6 +5,6 @@ from tasks import views
 urlpatterns = [
     path(r'send/', views.send, name='send'),
     path(r'result/', views.result, name='result-list'),
-    re_path(r'result/(?P<pk>\d+)', views.result, name='result-id'),
+    re_path(r'result/(?P<task_id>\s+)', views.result, name='result-id'),
     path(r'', views.home, name='home'),
 ]
